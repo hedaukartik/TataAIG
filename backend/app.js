b09ffc0f65b91a9e23176ad938dd0e5f7e6c35cf
hedaukartik <hedaukartik2009@gmail.com>
2020-08-31 21:56:47 +0530

    signup and signin api base

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan"); //for development purpose to get the routes requested in console
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
+//import routes
+const userRoutes = require("./routes/user");
 
 //app
 const app = express();
@@ -14,9 +19,13 @@ mongoose
 	})
 	.then(() => console.log("DB connected"));
 
-app.get("/", (req, res) => {
-	res.send("Hello from Node");
-});
+//middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//routes middleware
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
---
+++ backend/controllers/user.js
@@ -0,0 +1,45 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken"); //to generate signed token
+const expressJwt = require("express-jwt"); //for authorization check
+
+exports.signup = (req, res) => {
+	//console.log(req.body);
+	const user = new User(req.body);
+	user.save((err, user) => {
+		if (err) {
+			return res.status(400).json({
+				err,
+			});
+		}
+		res.json({
+			user,
+		});
+	});
+};
+
+exports.signin = (req, res) => {
+	//console.log(req.body);
+	//fing the user based on email
+	const { email, password } = req.body;
+	User.findOne({ email }, (err, user) => {
+		if (err || !user) {
+			return res.status(400).json({
+				error: "User with that email doesnt exist. Please signup",
+			});
+		}
+		//if user is found make sure the email and password both matches
+		//create authenticate method in user model for this
+		if (!user.authenticate(password)) {
+			return res.status(401).json({
+				error: "Email and password dont match",
+			});
+		}
+		//generate a signed token with userId and secret
+		const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+		//presist the token as t in cookie with expiry date
+		res.cookie("t", token, { expire: new Date() + 9999 }); //seconds
+
+		const { _id, name, email } = user;
+		return res.json({ token, user: { _id, name, email } });
+	});
+};

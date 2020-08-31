3d7c48284bd8c7e8342a4520f3ed9430f1147b90
hedaukartik <hedaukartik2009@gmail.com>
2020-08-31 16:10:22 +0530

    frontend and backend project build starter intital checkin

---
+++ backend/src/test/java/com/court/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.court;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

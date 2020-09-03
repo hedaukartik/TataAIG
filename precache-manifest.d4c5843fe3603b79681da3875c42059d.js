355ebf2b31caf256cd6812d20faac3a158c5b676
hedaukartik <hedaukartik2009@gmail.com>
2020-09-03 22:54:53 +0530

    Updates

---
+++ precache-manifest.d4c5843fe3603b79681da3875c42059d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07619ff7fdd07d0fb02caae4608c2a3b",
+    "url": "/TataAIG/index.html"
+  },
+  {
+    "revision": "0dc2e38cbc6e54d8e280",
+    "url": "/TataAIG/static/css/2.34a1685c.chunk.css"
+  },
+  {
+    "revision": "a5f9a7f4380b5201c527",
+    "url": "/TataAIG/static/css/main.ef8575dc.chunk.css"
+  },
+  {
+    "revision": "0dc2e38cbc6e54d8e280",
+    "url": "/TataAIG/static/js/2.572e41fd.chunk.js"
+  },
+  {
+    "revision": "c3cb654a3a7d27a576aed8d503b856d8",
+    "url": "/TataAIG/static/js/2.572e41fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f9a7f4380b5201c527",
+    "url": "/TataAIG/static/js/main.9291003f.chunk.js"
+  },
+  {
+    "revision": "486bbc3d94849948d844",
+    "url": "/TataAIG/static/js/runtime-main.39427e9f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/TataAIG/precache-manifest.d4c5843fe3603b79681da3875c42059d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TataAIG/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});

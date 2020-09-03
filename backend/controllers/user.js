b5b644b0a6c223c14c6613c26e70e129be3f7515
hedaukartik <hedaukartik2009@gmail.com>
2020-09-03 23:44:33 +0530

    aws server date issue

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -21,12 +21,12 @@ exports.addMealForUser = (req, res) => {
 	const meal = new Meal(req.body);
 	const userId = req.params.userId;
 	console.log("updatedDate", typeof req.body.updatedDate);
-	const ISODate = moment(new Date(req.body.updatedDate)).format(
+	const ISODate = moment(req.body.updatedDate).format(
 		"YYYY-MM-DD[T00:00:00.000Z]"
 	);
 	console.log(ISODate);
 	meal.user = userId;
-	meal.updatedDate = new Date(ISODate);
+	meal.updatedDate = ISODate;
 	console.log(meal);
 	meal.save((err, meal) => {
 		if (err) {
@@ -60,9 +60,7 @@ exports.updateMealForUser = (req, res) => {
 	const mealId = req.params.mealId;
 	if (req.body.updatedDate) {
 		meal.updatedDate = new Date(
-			moment(new Date(req.body.updatedDate)).format(
-				"YYYY-MM-DD[T00:00:00.000Z]"
-			)
+			moment(req.body.updatedDate).format("YYYY-MM-DD[T00:00:00.000Z]")
 		);
 	}
 	Meal.findByIdAndUpdate(
@@ -88,9 +86,9 @@ exports.getAllMealsForUser = (req, res) => {
 	query.user = req.params.userId;
 	if (req.query.requestDate) {
 		console.log("requestDate", typeof req.query.requestDate);
-		const ISODate = moment(
-			new Date(parseInt(req.query.requestDate))
-		).format("YYYY-MM-DD[T00:00:00.000Z]");
+		const ISODate = moment(parseInt(req.query.requestDate)).format(
+			"YYYY-MM-DD[T00:00:00.000Z]"
+		);
 		console.log("ISODate", ISODate);
 		query.updatedDate = ISODate;
 	}
